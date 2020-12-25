0c19203ab3928c6942619784fa974aba57c2bdfe
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2020-12-25 12:36:29 +0200

    Added connection to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
-//routes
+//Middlewares
+app.use('/posts', () => {
+    console.log("This is a middleware running!");
+});
+
+
+//Routes
 app.get('/', (req, res) => {
     res.send("Hello, home!");
 });
 
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => {console.log("Connected to DB");
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
+
+
+//Listening to the server
 app.listen(3000);
