eef5f780c1940f8c30d2e333f3e64ce3255386d6
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2020-12-25 15:04:30 +0200

    Added cors for cross-platform fetching

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(bodyParser.json()); //make the data readable
 
 //import routes
